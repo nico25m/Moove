59b48edbba8ead929bd1392d8fb80778bb61977b
nico25m <nickmelzi105@gmail.com>
2025-11-25 17:18:42 +0100

    update

--- moove.js
+++ moove.js
@@ -1,3 +1,14 @@
+var Logger = /** @class */ (function () {
+    function Logger() {
+    }
+    Logger.info = function (messaggio) {
+        console.log("[INFO]: " + messaggio);
+    };
+    Logger.errore = function (messaggio) {
+        console.log("[ERRORE]: " + messaggio);
+    };
+    return Logger;
+}());
 var Mezzo = /** @class */ (function () {
     function Mezzo(tipo, ID, stato) {
         this.tipo = tipo;
@@ -5,10 +16,12 @@ var Mezzo = /** @class */ (function () {
         this.stato = stato;
     }
     Mezzo.prototype.assegnaUtente = function (utente) {
-        if (this.stato == "disponibile") {
-            this.stato = "in uso";
-            console.log(this.tipo + " " + this.ID + " in uso");
+        if (this.stato === "in uso") {
+            Logger.errore(this.tipo + " " + this.ID + " è già in uso!");
+            return;
         }
+        this.stato = "in uso";
+        Logger.info(utente.nome + " ha preso in uso " + this.tipo + " " + this.ID + ".");
     };
     return Mezzo;
 }());
@@ -20,47 +33,41 @@ var Utente = /** @class */ (function () {
         this.metodoPagamento = metodoPagamento;
     }
     Utente.prototype.prenotaMezzo = function (mezzo) {
-        mezzo.assegnaUtente(this);
+        try {
+            mezzo.assegnaUtente(this);
+        }
+        catch (e) {
+            Logger.errore(e.message);
+        }
     };
     return Utente;
 }());
 var Citta = /** @class */ (function () {
     function Citta(nome, mezziDisponibili) {
+        if (mezziDisponibili === void 0) { mezziDisponibili = []; }
         this.nome = nome;
         this.mezziDisponibili = mezziDisponibili;
     }
     Citta.prototype.aggiungiMezzo = function (mezzo) {
-        this.mezziDisponibili.push(mezzo);
-        if (mezzo.stato == "disponibile") {
-            console.log(mezzo.tipo + " " + mezzo.ID + " aggiunto a " + this.nome + ".");
+        for (var i = 0; i < this.mezziDisponibili.length; i++) {
+            if (this.mezziDisponibili[i].ID === mezzo.ID) {
+                Logger.errore(mezzo.tipo + " " + mezzo.ID + " è già presente a " + this.nome + ".");
+                return;
+            }
         }
+        this.mezziDisponibili.push(mezzo);
+        Logger.info(mezzo.tipo + " " + mezzo.ID + " aggiunto a " + this.nome + ".");
     };
     return Citta;
 }());
 var bici1 = new Mezzo("Bici", 1, "disponibile");
-var bici2 = new Mezzo("Bici", 2, "disponibile");
 var scooter1 = new Mezzo("Scooter", 10, "disponibile");
-var scooter2 = new Mezzo("Scooter", 11, "disponibile");
-var mono1 = new Mezzo("Monopattino", 20, "disponibile");
-var mono2 = new Mezzo("Monopattino", 21, "disponibile");
 var utente1 = new Utente("Giuseppe", "Verdi", "g.verdi@gmail.com", "Mastercard");
 var utente2 = new Utente("Luca", "Rossi", "l.rossi@gmail.com", "Visa");
-var utente3 = new Utente("Maria", "Bianchi", "m.bianchi@gmail.com", "PayPal");
-var milano = new Citta("Milano", []);
-var roma = new Citta("Roma", []);
+var milano = new Citta("Milano");
 milano.aggiungiMezzo(bici1);
 milano.aggiungiMezzo(scooter1);
-milano.aggiungiMezzo(mono1);
-roma.aggiungiMezzo(bici2);
-roma.aggiungiMezzo(scooter2);
-roma.aggiungiMezzo(mono2);
-console.log("Milano:", milano);
-console.log("Roma:", roma);
+milano.aggiungiMezzo(bici1);
 utente1.prenotaMezzo(bici1);
 utente2.prenotaMezzo(bici1);
-utente3.prenotaMezzo(scooter1);
-console.log("Stato bici1:", bici1);
-console.log("Stato scooter1:", scooter1);
-var nuovoMono = new Mezzo("Monopattino", 30, "disponibile");
-milano.aggiungiMezzo(nuovoMono);
-console.log("Nuovo mezzo aggiunto a Milano:", milano.mezziDisponibili);
+utente2.prenotaMezzo(scooter1);
--- moove.ts
+++ moove.ts
@@ -1,3 +1,12 @@
+class Logger {
+  static info(messaggio: string) {
+    console.log("[INFO]: " + messaggio);
+  }
+  static errore(messaggio: string) {
+    console.log("[ERRORE]: " + messaggio);
+  }
+}
+
 interface IMezzo {
   tipo: "Bici" | "Scooter" | "Monopattino";
   ID: number;
@@ -24,17 +33,26 @@ class Mezzo implements IMezzo {
   ID: number;
   stato: "disponibile" | "in uso";
 
-  constructor(tipo: "Bici" | "Scooter" | "Monopattino", ID: number, stato: "disponibile" | "in uso") {
+  constructor(
+    tipo: "Bici" | "Scooter" | "Monopattino",
+    ID: number,
+    stato: "disponibile" | "in uso"
+  ) {
     this.tipo = tipo;
     this.ID = ID;
     this.stato = stato;
   }
 
   assegnaUtente(utente: IUtente): void {
-    if (this.stato == "disponibile") {
-      this.stato = "in uso";
-      console.log(this.tipo + " " + this.ID + " in uso");
+    if (this.stato === "in uso") {
+      Logger.errore(this.tipo + " " + this.ID + " è già in uso!");
+      return;
     }
+
+    this.stato = "in uso";
+    Logger.info(
+      utente.nome + " ha preso in uso " + this.tipo + " " + this.ID + "."
+    );
   }
 }
 
@@ -44,7 +62,12 @@ class Utente implements IUtente {
   email: string;
   metodoPagamento: string;
 
-  constructor(nome: string, cognome: string, email: string, metodoPagamento: string ) {
+  constructor(
+    nome: string,
+    cognome: string,
+    email: string,
+    metodoPagamento: string
+  ) {
     this.nome = nome;
     this.cognome = cognome;
     this.email = email;
@@ -52,7 +75,11 @@ class Utente implements IUtente {
   }
 
   prenotaMezzo(mezzo: IMezzo): void {
-    mezzo.assegnaUtente(this);
+    try {
+      mezzo.assegnaUtente(this);
+    } catch (e: any) {
+      Logger.errore(e.message);
+    }
   }
 }
 
@@ -60,53 +87,44 @@ class Citta implements ICitta {
   nome: string;
   mezziDisponibili: IMezzo[];
 
-  constructor(nome: string, mezziDisponibili: IMezzo[]) {
+  constructor(nome: string, mezziDisponibili: IMezzo[] = []) {
     this.nome = nome;
     this.mezziDisponibili = mezziDisponibili;
   }
 
   aggiungiMezzo(mezzo: IMezzo): void {
-    this.mezziDisponibili.push(mezzo);
-    if (mezzo.stato == "disponibile") {
-      console.log(mezzo.tipo + " " + mezzo.ID + " aggiunto a "+ this.nome + ".");
+    for (let i = 0; i < this.mezziDisponibili.length; i++) {
+      if (this.mezziDisponibili[i].ID === mezzo.ID) {
+        Logger.errore(
+          mezzo.tipo + " " + mezzo.ID + " è già presente a " + this.nome + "."
+        );
+        return;
+      }
     }
+    this.mezziDisponibili.push(mezzo);
+    Logger.info(mezzo.tipo + " " + mezzo.ID + " aggiunto a " + this.nome + ".");
   }
 }
 
 const bici1 = new Mezzo("Bici", 1, "disponibile");
-const bici2 = new Mezzo("Bici", 2, "disponibile");
-
 const scooter1 = new Mezzo("Scooter", 10, "disponibile");
-const scooter2 = new Mezzo("Scooter", 11, "disponibile");
 
-const mono1 = new Mezzo("Monopattino", 20, "disponibile");
-const mono2 = new Mezzo("Monopattino", 21, "disponibile");
-
-const utente1 = new Utente("Giuseppe","Verdi","g.verdi@gmail.com","Mastercard");
+const utente1 = new Utente(
+  "Giuseppe",
+  "Verdi",
+  "g.verdi@gmail.com",
+  "Mastercard"
+);
 const utente2 = new Utente("Luca", "Rossi", "l.rossi@gmail.com", "Visa");
-const utente3 = new Utente("Maria", "Bianchi", "m.bianchi@gmail.com", "PayPal");
 
-const milano = new Citta("Milano", []);
-const roma = new Citta("Roma", []);
+const milano = new Citta("Milano");
 
 milano.aggiungiMezzo(bici1);
 milano.aggiungiMezzo(scooter1);
-milano.aggiungiMezzo(mono1);
-
-roma.aggiungiMezzo(bici2);
-roma.aggiungiMezzo(scooter2);
-roma.aggiungiMezzo(mono2);
-
-console.log("Milano:", milano);
-console.log("Roma:", roma);
+milano.aggiungiMezzo(bici1);
 
 utente1.prenotaMezzo(bici1);
+
 utente2.prenotaMezzo(bici1);
-utente3.prenotaMezzo(scooter1); 
 
-console.log("Stato bici1:", bici1);
-console.log("Stato scooter1:", scooter1);
-
-const nuovoMono = new Mezzo("Monopattino", 30, "disponibile");
-milano.aggiungiMezzo(nuovoMono);
-console.log("Nuovo mezzo aggiunto a Milano:", milano.mezziDisponibili);
+utente2.prenotaMezzo(scooter1);
